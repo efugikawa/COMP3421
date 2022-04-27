1be6a1e1615eeb91e06a4b1435022bcedc1ae0c9
efugikawa <elizabeth.fugikawa@gmail.com>
2022-04-27 14:00:03 -0600

    all tables generated

--- datagenerator.py
+++ datagenerator.py
@@ -18,14 +18,14 @@ numCustomers = 10
 numCompanies = 5
 
 
-#agents
+#Agents: agent ID, agent name
 outfile = open("agents_data", "w")
 for i in range(len(agentNames)):
     s = str(i+1) + "\t" + agentNames[i] + "\n"
     outfile.write(s)
 outfile.close()
 
-#tickets
+#Tickets: ticket ID, category, time opened, time closed
 outfile = open("tickets_data", "w")
 startDate = datetime.date(2021, 1, 1)
 endDate = datetime.date(2021, 12, 31)
@@ -37,7 +37,7 @@ for i in range(numTickets):
 outfile.close()
 
 
-#customers
+#Customers: customer ID, customer name, job title
 outfile = open("customers_data", "w")
 for i in range(numCustomers):
     s = str(i+1) + "\t" + fake.name() + "\t" + random.choice(jobs) + "\n"
@@ -45,9 +45,41 @@ for i in range(numCustomers):
 outfile.close()
 
 
-#companies
+#Companies: company ID, company name, industry
 outfile = open("companies_data", "w")
 for i in range(numCompanies):
     s = str(i+1) + "\t" + fake.company() + "\t" + random.choice(industries) + "\n"
+    outfile.write(s)
+outfile.close()
+
+
+#Handles: agent id, ticket id, rating
+outfile = open("handles_data", "w")
+for i in range(numTickets):
+    s = str(random.randint(1, len(agentNames))) + "\t" + str(i+1) + "\t" + str(random.choice(ratings)) + "\n"
+    outfile.write(s)
+outfile.close()
+
+
+#Opens: customer ID, ticketID
+outfile = open("opens_data", "w")
+for i in range(numTickets):
+    s = str(random.randint(1, numCustomers)) + "\t" + str(i+1) + "\n"
+    outfile.write(s)
+outfile.close()
+
+
+#Works At: customer ID, company ID
+outfile = open("worksat_data", "w")
+for i in range(numCustomers):
+    s = str(i+1) + "\t" + str(random.randint(1,numCompanies)) + "\n"
+    outfile.write(s)
+outfile.close()
+
+
+#Manages: agent ID, company ID, health
+outfile = open("manages_data", "w")
+for i in range(numCompanies):
+    s = str(random.randint(1, len(agentNames))) + "\t" + str(i+1) + "\t" + random.choice(healthScores) + "\n"
     outfile.write(s)
 outfile.close()
