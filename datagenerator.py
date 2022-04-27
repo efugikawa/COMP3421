f8cf78ed3bceb107c1306eb36a2cf4dfc6436938
efugikawa <elizabeth.fugikawa@gmail.com>
2022-04-27 14:45:24 -0600

    assn 4 submission

--- datagenerator.py
+++ datagenerator.py
@@ -13,20 +13,20 @@ jobs = ["surveyor", "pilot", "foreman", "manager", "owner"]
 categories = ["troubleshooting", "renewal", "training"]
 
 #change these numbers to change how much data you generate
-numTickets = 15
-numCustomers = 10
-numCompanies = 5
+numTickets = 15000
+numCustomers = 10000
+numCompanies = 500
 
 
 #Agents: agent ID, agent name
-outfile = open("agents_data", "w")
+outfile = open("agents_data.txt", "w")
 for i in range(len(agentNames)):
     s = str(i+1) + "\t" + agentNames[i] + "\n"
     outfile.write(s)
 outfile.close()
 
 #Tickets: ticket ID, category, time opened, time closed
-outfile = open("tickets_data", "w")
+outfile = open("tickets_data.txt", "w")
 startDate = datetime.date(2021, 1, 1)
 endDate = datetime.date(2021, 12, 31)
 for i in range(numTickets):
@@ -38,7 +38,7 @@ outfile.close()
 
 
 #Customers: customer ID, customer name, job title
-outfile = open("customers_data", "w")
+outfile = open("customers_data.txt", "w")
 for i in range(numCustomers):
     s = str(i+1) + "\t" + fake.name() + "\t" + random.choice(jobs) + "\n"
     outfile.write(s)
@@ -46,7 +46,7 @@ outfile.close()
 
 
 #Companies: company ID, company name, industry
-outfile = open("companies_data", "w")
+outfile = open("companies_data.txt", "w")
 for i in range(numCompanies):
     s = str(i+1) + "\t" + fake.company() + "\t" + random.choice(industries) + "\n"
     outfile.write(s)
@@ -54,7 +54,7 @@ outfile.close()
 
 
 #Handles: agent id, ticket id, rating
-outfile = open("handles_data", "w")
+outfile = open("handles_data.txt", "w")
 for i in range(numTickets):
     s = str(random.randint(1, len(agentNames))) + "\t" + str(i+1) + "\t" + str(random.choice(ratings)) + "\n"
     outfile.write(s)
@@ -62,7 +62,7 @@ outfile.close()
 
 
 #Opens: customer ID, ticketID
-outfile = open("opens_data", "w")
+outfile = open("opens_data.txt", "w")
 for i in range(numTickets):
     s = str(random.randint(1, numCustomers)) + "\t" + str(i+1) + "\n"
     outfile.write(s)
@@ -70,7 +70,7 @@ outfile.close()
 
 
 #Works At: customer ID, company ID
-outfile = open("worksat_data", "w")
+outfile = open("worksat_data.txt", "w")
 for i in range(numCustomers):
     s = str(i+1) + "\t" + str(random.randint(1,numCompanies)) + "\n"
     outfile.write(s)
@@ -78,7 +78,7 @@ outfile.close()
 
 
 #Manages: agent ID, company ID, health
-outfile = open("manages_data", "w")
+outfile = open("manages_data.txt", "w")
 for i in range(numCompanies):
     s = str(random.randint(1, len(agentNames))) + "\t" + str(i+1) + "\t" + random.choice(healthScores) + "\n"
     outfile.write(s)
